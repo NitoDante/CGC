c26b427b570af2f119e6f9d349a116f2182c031d
NitoDante <antonio_mh@outlook.com>
2022-04-14 20:09:00 +0200

    Menu bar v.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewsComponent } from './news/news.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { RulesComponent } from './rules/rules.component';
+import { VideosComponent } from './videos/videos.component';
+
+const routes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'ranking', component: RankingComponent },
+  { path: 'rules', component: RulesComponent },
+  { path: 'videos', component: VideosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { NewsComponent } from './news/news.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { VideosComponent } from './videos/videos.component';
+import { ContactComponent } from './contact/contact.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    CalendarComponent,
+    NewsComponent,
+    RankingComponent,
+    VideosComponent,
+    ContactComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
